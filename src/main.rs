e074ae2cc66cf50b4f41547c822c5f439f7a1f9f
Mark Du <dulongjie@gmail.com>
2024-09-09 15:29:25 -0400

    setup custom test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_main"]
+
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
@@ -27,6 +34,25 @@ pub extern "C" fn _start() -> !{
 // vga_buffer::WRITER.lock().write_string("Hello here");
 
 println!("Hello {}", "...");
+// panic!("here is a panic coming...");
+
+#[cfg(test)]
+test_main();
 
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests...", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn demo_assertion() {
+    print!("an assertion demo...");
+    assert_eq!(1,1);
+    println!("[OK]");
 }
