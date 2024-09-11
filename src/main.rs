66c596b12e4011200ec7fb6e8329ea2bc11be200
Mark Du <dulongjie@gmail.com>
2024-09-11 10:16:36 -0400

    breakpoint  exception

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ use mini_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Welcome to MINI_OS");
 
+    // init for interrupt
+    mini_os::init();
+
+    // invoke a breakpoint interruption
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
