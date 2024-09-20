7fcfcdca3cc2b0888c80e14323f15bc3ebc60d71
Mark Du <dulongjie@gmail.com>
2024-09-20 11:21:22 -0400

    pc-keyboard crate map keys

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -85,12 +85,41 @@ extern "x86-interrupt" fn timer_interrupt_handler( _stack_frame: InterruptStackF
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame){
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    // HandleControl for ctrl + [a-z]
+    lazy_static!{
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = 
+            Mutex::new(Keyboard::new(ScancodeSet1::new(), layouts::Us104Key, HandleControl::Ignore));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe {
+        port.read()
+    };
+
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character), //map ctrl + [a-z]
+                DecodedKey::RawKey(key) => print!("{:?}",key), //
+            }
+        }
+    }
+
+
+ /*    
     use x86_64::instructions::port::Port;
 
     let mut port = Port::new(0x60);
     let scancode: u8 = unsafe {
         port.read()
     };
+
     // print!("keyboard {}", scancode);
 
     let key = match scancode {
@@ -110,7 +139,7 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     if let Some(key) = key {
         print!("{}", key);
     }
-
+*/
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
