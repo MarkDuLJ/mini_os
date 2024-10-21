4274a5cc88f272817e548f911d08ede6babd92ed
Mark Du <dulongjie@gmail.com>
2024-10-21 12:35:08 -0400

    freestanding w/o os

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("hellow world!");
+#![no_std]
+#![no_main]
+
+// use core::panic::PanicInfo;
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {
+        
+    }
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> !{
+    loop {
+        
+    }
 }
