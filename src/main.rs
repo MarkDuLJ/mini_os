c94415598f782daf353aae571cf9f2492ddbda1f
Mark Du <dulongjie@gmail.com>
2024-10-21 15:17:41 -0400

    created the bootimage

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,20 @@
 #![no_main]
 
 // use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe {
+            *vga_buf.offset(i as isize * 2) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xc;
+        }
+    }
+
     loop {
         
     }
