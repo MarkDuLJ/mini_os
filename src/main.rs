82d90970039d164a2214f265abe172397d795540
Mark Du <dulongjie@gmail.com>
2024-10-21 20:12:07 -0400

    print to vga text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #![no_std]
 #![no_main]
 
+mod vga_buf;
+
 // use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    /* 
     let vga_buf = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate(){
         unsafe {
             *vga_buf.offset(i as isize * 2) = byte;
-            *vga_buf.offset(i as isize * 2 + 1) = 0xc;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+    */
 
+    vga_buf::print_sth();
     loop {
         
     }
